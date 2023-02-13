5ef5bca78306d162c0476e4494abe4ab03e380a3
AnnemariaRe <annemariarepenko@gmail.com>
2023-02-13 17:24:56 +0300

    feat: show product info

--- TrackCaloriesBot/src/Command/SearchInlineQueryCommand.cs
+++ TrackCaloriesBot/src/Command/SearchInlineQueryCommand.cs
@@ -1,7 +1,10 @@
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.ReplyMarkups;
 using TrackCaloriesBot.Constant;
+using TrackCaloriesBot.Entity;
 using TrackCaloriesBot.Service.Interfaces;
 
 namespace TrackCaloriesBot.Command;
@@ -19,18 +22,59 @@ public class SearchInlineQueryCommand : ICommand
     private readonly ISpoonacularService _spoonacularService;
     public async Task Execute(Update? update, ITelegramBotClient client)
     {
-        var conversation = await _conversationService.GetAddProductConversation(update.InlineQuery.From.Id)!;
-        if (conversation?.ConversationStage is 1)
+        switch (update.Type)
         {
-            var productsResult = _spoonacularService.GetProducts(update.InlineQuery.Query);
-            var results = productsResult.Result
-                .Select(product =>
-                    new InlineQueryResultArticle(product.Title.GetHashCode().ToString(), product.Title,
-                        new InputTextMessageContent($"{product.ProductId}")) { ThumbUrl = product.Image })
-                .Cast<InlineQueryResult>().ToList();
-            
-            await client.AnswerInlineQueryAsync(
-                update.InlineQuery.Id, results);
+            case UpdateType.InlineQuery:
+            {
+                var conversation = await _conversationService.GetAddProductConversation(update.InlineQuery.From.Id)!;
+                if (conversation?.ConversationStage is 1)
+                {
+                    var productsResult = _spoonacularService.GetProducts(update.InlineQuery.Query);
+                    var results = productsResult.Result
+                        .Select(product =>
+                            new InlineQueryResultArticle(product.Title.GetHashCode().ToString(), product.Title,
+                                new InputTextMessageContent($"{product.ProductId}")) { ThumbUrl = product.Image })
+                        .Cast<InlineQueryResult>().ToList();
+                
+                    await client.AnswerInlineQueryAsync(
+                        update.InlineQuery.Id, results);
+                }
+
+                break;
+            }
+            case UpdateType.Message:
+            {
+                var conversation = await _conversationService.GetAddProductConversation(update.Message.From.Id)!;
+                switch (conversation?.ConversationStage)
+                {
+                    case 1:
+                        await _conversationService.IncrementStage(update.Message.From.Id);
+                        goto case 2;
+                    case 2:
+                        ResponseProduct? productResult = null;
+                        if (int.TryParse(update.Message.Text, out var x))
+                        {
+                            productResult = _spoonacularService.GetProductInfo(x).Result;
+                        }
+                        
+                        var message = $"<b>{productResult.Title}</b> \n" + "---------------------\n" +
+                                      $"<pre>Calories: {productResult.Nutrition.Nutrients.FirstOrDefault(x => x.Name == "Calories").Amount} kcal \n" +
+                                      $"Carbs:    {productResult.Nutrition.Nutrients.FirstOrDefault(x => x.Name == "Carbohydrates").Amount} g \n" +
+                                      $"Protein:  {productResult.Nutrition.Nutrients.FirstOrDefault(x => x.Name == "Protein").Amount} g \n" +
+                                      $"Fat:     {productResult.Nutrition.Nutrients.FirstOrDefault(x => x.Name == "Fat").Amount} g </pre>\n";
+
+                        await client.SendTextMessageAsync(
+                            chatId: update.Message.From.Id,
+                            text: message,
+                            parseMode: ParseMode.Html,
+                            replyMarkup: InlineKeyboards.ProductInfoInlineKeyboard);
+                        break;
+                }
+
+                break;
+            }
+            default:
+                throw new ArgumentOutOfRangeException();
         }
     }
 }
